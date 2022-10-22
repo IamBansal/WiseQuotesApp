2e77ae957db40c67b67240ccb8ab615cea145d22
IamBansal <abansal11ae@gmail.com>
2022-10-22 10:55:28 +0530

    Notifications Fragment

---
+++ app/src/main/java/com/example/wisequotesapp/ui/adapter/NotificationsAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.wisequotesapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wisequotesapp.databinding.CustomNotificationItemBinding
+import com.example.wisequotesapp.ui.model.Author
+
+class NotificationsAdapter(private val fragment: Fragment, private val list: List<Author>): RecyclerView.Adapter<NotificationsAdapter.ViewHolder>() {
+
+    class ViewHolder(view: CustomNotificationItemBinding) : RecyclerView.ViewHolder(view.root) {
+        val text: TextView = view.text
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(CustomNotificationItemBinding.inflate(LayoutInflater.from(fragment.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val author = list[position]
+        holder.text.text = author.text
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+}
--- app/src/main/java/com/example/wisequotesapp/ui/fragment/main/NotificationFragment.kt
+++ app/src/main/java/com/example/wisequotesapp/ui/fragment/main/NotificationFragment.kt
@@ -3,17 +3,21 @@ package com.example.wisequotesapp.ui.fragment.main
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wisequotesapp.R
 import com.example.wisequotesapp.databinding.FragmentNotificationBinding
 import com.example.wisequotesapp.ui.activity.MainActivity
+import com.example.wisequotesapp.ui.adapter.NotificationsAdapter
+import com.example.wisequotesapp.ui.model.Author
 
 class NotificationFragment : Fragment() {
 
     private lateinit var binding: FragmentNotificationBinding
+    private lateinit var list: List<Author>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,6 +53,21 @@ class NotificationFragment : Fragment() {
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         setupToolbar()
+
+        list = listOf(
+            Author("Notification 1"),
+            Author("Notification 2"),
+            Author("Notification 3"),
+            Author("Notification 4"),
+            Author("Notification 5"),
+            Author("Notification 5"),
+            Author("Notification 5"),
+            Author("Notification 5"),
+            Author("Notification 5"),
+            Author("Notification 10"),
+        )
+        binding.rvNotifications.layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvNotifications.adapter = NotificationsAdapter(this@NotificationFragment, list)
 
     }
 
