e1b845709d4f3f567296c3895dcb8d8ba49a35dc
IamBansal <abansal11ae@gmail.com>
2022-11-08 17:09:12 +0530

    UI Fix

--- app/src/main/java/com/example/wisequotesapp/ui/fragment/auth/SignupFragment.kt
+++ app/src/main/java/com/example/wisequotesapp/ui/fragment/auth/SignupFragment.kt
@@ -1,12 +1,18 @@
 package com.example.wisequotesapp.ui.fragment.auth
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.wisequotesapp.databinding.CustomBottomSheetBinding
 import com.example.wisequotesapp.databinding.FragmentSignupBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class SignupFragment : Fragment() {
 
@@ -27,6 +33,37 @@ class SignupFragment : Fragment() {
             findNavController().navigateUp()
         }
 
+        binding.signupButton.setOnClickListener {
+            showBottomSheetDialog()
+        }
+
+    }
+
+    private fun showBottomSheetDialog() {
+        val dialog = BottomSheetDialog(requireActivity())
+        val view = CustomBottomSheetBinding.inflate(layoutInflater)
+
+        view.numberOTP.text = binding.etPhone.text
+
+        dialog.setCancelable(true)
+        dialog.setContentView(view.root)
+        dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        view.submitOTP.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        object : CountDownTimer(30000, 1000) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(millisUntilFinished: Long) {
+                view.timer.text = "00:${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                dialog.dismiss()
+            }
+        }.start()
+
+        dialog.show()
     }
 
 }
