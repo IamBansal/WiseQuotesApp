bd05ca8234c1b704b477322b33b02ca37df5efd4
IamBansal <abansal11ae@gmail.com>
2022-11-18 12:13:22 +0530

    UI Fix

--- app/src/main/java/com/example/wisequotesapp/ui/fragment/main/ProfileFragment.kt
+++ app/src/main/java/com/example/wisequotesapp/ui/fragment/main/ProfileFragment.kt
@@ -5,20 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.wisequotesapp.R
+import com.example.wisequotesapp.databinding.FragmentProfileBinding
 import com.example.wisequotesapp.ui.activity.MainActivity
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(requireActivity()).load(R.drawable.model).circleCrop().into(binding.imageProfile)
 
         setupToolbar()
 
